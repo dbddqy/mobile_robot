2ad232ed6391cacc183a8a42b90dd3fdf5068080
Yue <dbddqy@gmail.com>
2020-05-16 17:37:29 +0200

    refined commander joy

--- catkin_ws/src/commander_joy/script/commander_joy.py
+++ catkin_ws/src/commander_joy/script/commander_joy.py
@@ -10,6 +10,11 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import Joy
 
 
+MIN_SPEED = 100.0 # min: 100 mm/s (or 0.1*pi rad/s)
+MAX_SPEED = 250.0 # max: 600 mm/s (or 0.6*pi rad/s)
+ITERATION_TO_CONTINUE = 0
+
+
 def serial_init():
     ser = serial.Serial("/dev/ttyAMA0",74880)
 
@@ -24,18 +29,32 @@ def serial_init():
 
 def callback_Joy(joy):
     global ser
-    speed_x = -joy.axes[0] * (1-joy.axes[5]) * 300.0
-    speed_y = +joy.axes[1] * (1-joy.axes[5]) * 300.0
-    speed_w = (1-joy.axes[2]) * (1-joy.axes[5]) * 0.15 * pi
-    float_bytes = pack('fff', speed_x, speed_y, speed_w)
-    ser.write(float_bytes)
+    global speed_x, speed_y, speed_w, ITERATION_TO_CONTINUE
+    ITERATION_TO_CONTINUE = 200
+    speed_x = -joy.axes[0] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED)
+    speed_y = +joy.axes[1] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED)
+    speed_w = +joy.axes[3] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED) * 0.001 * pi # max: 0.6*pi rad/s
+    
 
-
-ser = serial_init()
+speed_x = 0.0
+speed_y = 0.0
+speed_w = 0.0
 
 rospy.init_node("commander_joy", anonymous=False)
+ser = serial_init()
+
 rospy.Subscriber('joy', Joy, callback_Joy)
 
 rate = rospy.Rate(25)
-rospy.spin()
+while not rospy.is_shutdown():
+    if ITERATION_TO_CONTINUE > 0:
+        ITERATION_TO_CONTINUE -= 1
+    else:
+        speed_x = 0.0
+        speed_y = 0.0
+        speed_w = 0.0
+    rospy.loginfo("x: %f  y: %f  w: %f", speed_x, speed_y, speed_w)
+    float_bytes = pack('fff', speed_x, speed_y, speed_w)
+    ser.write(float_bytes)
+    rate.sleep()
 
