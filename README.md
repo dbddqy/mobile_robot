0fd079bb247a4cae55040bf90ad37fa3bb6264b5
Yue <dbddqy@gmail.com>
2020-07-22 06:34:13 +0200

    slam

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 Developing mobile robotic platform.
 
-# Result of first stage development  
-### test chassis  
-![](pics/test_chassis.gif)
-### test odometry  
+# Testing Results
+
+### gmapping  
+![](pics/test_gmapping.gif)
+
+### odometry  
 ![](pics/test_odom.gif)
 ![](pics/test_odom_video.gif)
 
+### chassis  
+![](pics/test_chassis.gif)
+
 ## Issues
 
-PID not fine tuned. Can only reach (required_speed - 20 mm/s)
+- [x] PID not fine tuned. Can only reach (required_speed - 20 mm/s)
 
 
 ## Low level control
@@ -36,5 +41,5 @@ Developing laser-based SLAM in ROS.
 - [ ] implement ros-control package
 - [ ] implement gmapping package in Gazebo simulation environment
 - [ ] implement navigation related package in Gazebo simulation environment
-- [ ] implement gmapping package on real robot
+- [x] implement gmapping package on real robot
 - [ ] implement navigation related package on real robot
--- catkin_ws/src/commander/script/commander_joy.py
+++ catkin_ws/src/commander/script/commander_joy.py
@@ -11,18 +11,21 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 
 
-MIN_SPEED = 100.0 # min: 100 mm/s (or 0.1*pi rad/s)
-MAX_SPEED = 250.0 # max: 250 mm/s (or 0.25*pi rad/s)
+MIN_SPEED = 0.15 # min: 100 mm/s (or 0.1*pi rad/s)
+MAX_SPEED = 0.25 # max: 250 mm/s (or 0.25*pi rad/s)
+
+ITERATION_TO_CONTINUE = 0
 
 speed_x = 0.0
 speed_y = 0.0
 speed_w = 0.0
 
 def callback_Joy(joy):
-    global speed_x, speed_y, speed_w
+    global speed_x, speed_y, speed_w, ITERATION_TO_CONTINUE
+    ITERATION_TO_CONTINUE = 25
     speed_x = -joy.axes[0] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED)
     speed_y = +joy.axes[1] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED)
-    speed_w = +joy.axes[3] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED) * 0.001 * pi
+    speed_w = +joy.axes[3] * ((0.5-0.5*joy.axes[5]) * (MAX_SPEED - MIN_SPEED) + MIN_SPEED)
 
 
 rospy.init_node("commander_joy", anonymous=False)
@@ -30,8 +33,14 @@ rospy.init_node("commander_joy", anonymous=False)
 pub = rospy.Publisher('vel_command', Twist , queue_size=10)
 rospy.Subscriber("joy", Joy, callback_Joy)
 
-rate = rospy.Rate(5)
+rate = rospy.Rate(25)
 while not rospy.is_shutdown():
+    if ITERATION_TO_CONTINUE > 0:
+        ITERATION_TO_CONTINUE -= 1
+    else:
+        speed_x = 0.0
+        speed_y = 0.0
+        speed_w = 0.0
     msg = Twist()
     msg.linear.x = speed_x
     msg.linear.y = speed_y
